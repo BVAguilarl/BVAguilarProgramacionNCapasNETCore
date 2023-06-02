4b9a0aad54a9b28238cadfe4e5ea5a3cb1e9d283
Brenda V. Aguilar <bvanessaal99@gmail.com>
2023-06-02 15:19:53 -0600

    Add project files.

---
+++ DL/Usuario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DL;
+
+public partial class Usuario
+{
+    public int IdUsuario { get; set; }
+
+    public string Curp { get; set; } = null!;
+
+    public string Nombre { get; set; } = null!;
+
+    public string ApellidoPaterno { get; set; } = null!;
+
+    public string? ApellidoMaterno { get; set; }
+
+    public string EmailEmpresarial { get; set; } = null!;
+
+    public string Sexo { get; set; } = null!;
+
+    public string Telefono { get; set; } = null!;
+
+    public string Celular { get; set; } = null!;
+
+    public DateTime? FechaDeNacimiento { get; set; }
+
+    public DateTime? FechaDeRegistro { get; set; }
+
+    public byte[]? Foto { get; set; }
+
+    public int? IdArea { get; set; }
+
+    public int? IdUsuarioModificacion { get; set; }
+
+    public virtual Area? IdAreaNavigation { get; set; }
+
+    public virtual Usuario? IdUsuarioModificacionNavigation { get; set; }
+
+    public virtual ICollection<Usuario> InverseIdUsuarioModificacionNavigation { get; set; } = new List<Usuario>();
+
+    public virtual ICollection<Producto> Productos { get; set; } = new List<Producto>();
+
+    public virtual ICollection<UsuarioFecha> UsuarioFechas { get; set; } = new List<UsuarioFecha>();
+
+    public virtual ICollection<UsuarioProducto> UsuarioProductoIdUsuarioModificacionNavigations { get; set; } = new List<UsuarioProducto>();
+
+    public virtual ICollection<UsuarioProducto> UsuarioProductoIdUsuarioNavigations { get; set; } = new List<UsuarioProducto>();
+}
