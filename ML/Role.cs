98e777fa8eb659bf4e59b052f7ff36726b4c6363
Brenda V. Aguilar <bvanessaal99@gmail.com>
2023-06-06 19:11:18 -0600

    rol inser, delete falta update controlador y vistas de rol program.cs se agrega rol

---
+++ ML/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace ML
+{
+    public class Role
+    {
+        public string IdRol { get; set; }
+        public string Name { get; set; }
+        public List<object> Roles { get; set; }
+    }
+}
---
+++ PL/Controllers/RoleController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PL.Controllers
+{
+    public class RolController : Controller
+    {
+        private RoleManager<IdentityRole> roleManager;
+        public RolController(RoleManager<IdentityRole> roleMgr)
+        {
+            roleManager = roleMgr;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var Roles = roleManager.Roles.ToList();
+
+            return View(Roles);
+        }
+        public IActionResult Form(Guid? Id)
+        {
+            IdentityRole role = new IdentityRole();
+
+            return View(role);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Form([Required] IdentityRole rol)
+        {
+            if (ModelState.IsValid)
+            {
+                var rolExist = await roleManager.FindByIdAsync(rol.Id); //existencia de un rol
+                IdentityResult result;
+
+                if (rolExist == null) //no existe
+                {
+                    //add
+                    result = await roleManager.CreateAsync(new IdentityRole(rol.Name));
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("GetAll");
+                    }
+                   
+                }
+                else
+                {
+                    if (rolExist.Name != rol.Name)
+                    {
+                        rolExist.Name = rol.Name;
+                    }
+                        
+
+                    result = await roleManager.UpdateAsync(rolExist);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("GetAll", "Rol", new { Id = rol.Id });
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "No se puede realizar la accion");
+                    }
+                                                  
+                }
+                               
+            }
+            return View(rol);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid? Id)
+        {
+            var role = await roleManager.FindByIdAsync(Id.ToString());
+            
+            if (role != null)
+            {
+                var result = await roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {  
+                    return RedirectToAction("GetAll");
+                }                    
+            }
+            else
+            {
+                ModelState.AddModelError("", "No se puede realizar la accion");
+            }
+               
+            return View(role);
+
+        }
+
+    }
+}
